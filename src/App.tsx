1b811f43578360252f37de3d7e576ca81aca8693
Dmitry <melkdim@list.ru>
2025-04-13 15:29:50 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>test</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus reiciendis alias soluta quod, velit
+          laudantium, incidunt ad, sequi similique debitis optio. Cumque animi ducimus earum ex a commodi, nesciunt
+          enim.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
